e8a42791b02ff5f0c73a5d6db1869598c79e527f
ReDay Zarra <redayzarra@gmail.com>
2023-08-06 05:49:45 -0400

    Cleaning up math file code

--- Maths.cpp
+++ Maths.cpp
@@ -5,17 +5,17 @@ int main() {
     int y = 3;
     int z = x + y;
 
-    int z = x / y;      // Both numbers are int so int division
+    //int z = x / y;      // Both numbers are int so int division
 
-    double x = 10;
-    double z = x / y;   // Now the result will be a float
+    //double x = 10;
+    //double z = x / y;   // Now the result will be a float
 
     y = x++;    // This increments x, you can decrement with x--;
                 // First, y = x and then x will be incremented
 
     z = ++x;    // X will increment and then z will become x;
 
-    double x = 1 + 2 * 3;    // Multiplication and divison are higher
+    //double x = 1 + 2 * 3;    // Multiplication and divison are higher
 
     double a = 10;
     double b = 5;
