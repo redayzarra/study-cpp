2c356df7e855ba751f73c64afd9f360730f6cceb
ReDay Zarra <redayzarra@gmail.com>
2023-08-06 05:25:37 -0400

    Learning to assign different numerical variables

---
+++ Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int file_size = 100;
+
+    double sales = 99.9;
+
+    int a = 1;
+    int b = 2;
+    int temp = a;
+
+    a = b;
+    b = temp;
+
+    std::cout << "Hello World\n";
+
+    return 0;
+}
