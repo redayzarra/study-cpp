925bc822d36b434c2e05ac279321144a64f52a3c
ReDay Zarra <redayzarra@gmail.com>
2023-08-07 05:02:17 -0400

    Learning how to accept and manipulate user input

---
+++ Input.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter values for x and y: ";
+
+    double x;
+    double y;
+
+    cin >> x >> y;
+
+    cout << x + y;
+
+    cout << "Enter a value in Celsius: ";
+    int celsius;
+    cin >> celsius;
+    double fahrenheit = (celsius * 1.8) + 32;
+    cout << fahrenheit;
+}
