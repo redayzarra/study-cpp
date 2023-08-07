2fc5ade9507a806809cfae4bbe43fbac51c9f89f
ReDay Zarra <redayzarra@gmail.com>
2023-08-07 05:32:52 -0400

    Learning the different types of numerical values

--- Variables.cpp
+++ Variables.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    int file_size = 100;
-
-    double sales = 99.9;
-
-    int a = 1;
-    int b = 2;
-    int temp = a;
-
-    a = b;
-    b = temp;
 
     double price = 99.99;
     float interestRate = 3.67f;
     long fileSize = 90000L;
     char letter = 'a';
     bool isValid = true;
-    return 0;
+    
+    int number = 0b11111111;
+    cout << number << price << interestRate 
+         << fileSize << letter << isValid;
 }
