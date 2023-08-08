63a4d7b34167a8c2770bee92fd4ed737edfa6d73
ReDay Zarra <redayzarra@gmail.com>
2023-08-08 05:10:50 -0400

    Generating random dice values

--- Random.cpp
+++ Random.cpp
@@ -5,8 +5,16 @@
 using namespace std;
 
 int main() {
-    long elapsedSeconds = time(nullptr); 
+    long elapsedSeconds = time(nullptr); // January 1st 1970
     srand(elapsedSeconds);
     int number = rand() % 10;
-    cout << number;
+    cout << number << endl;
+
+    // Excercise
+    const short minValue = 1;
+    const short maxValue = 6;
+    short first = (rand() % (maxValue - minValue + 1)) + minValue;
+    short second = (rand() % (maxValue - minValue + 1)) + minValue;
+
+    cout << first << ", " << second << endl;
 }
