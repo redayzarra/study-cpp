5992ec4100aa7d29e375b0536ad83443e567129c
ReDay Zarra <redayzarra@gmail.com>
2023-08-10 05:23:22 -0400

    Learning how arrays are initialized

---
+++ Arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Allocate a static array of 5 integers. They are all zero's rn.
+    int array[5] = {10, 20};
+
+    string names[3];
+
+    cout << "Name: " << endl;
+    getline(cin, names[0]);
+    cout << "Name: " << endl;
+    getline(cin, names[1]);
+    cout << "Name: " << endl;
+    getline(cin, names[2]);
+
+    cout << names[0] << endl  
+         << array << endl;
+}
