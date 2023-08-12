2545f1521efa3e68b5807794a4f42a72210d747f
ReDay Zarra <redayzarra@gmail.com>
2023-08-12 16:47:01 -0400

    Learning all major operators

--- Logical.cpp
+++ Logical.cpp
@@ -7,5 +7,11 @@ int main() {
 
     // And operator
     bool isOkay = age > 18 && age < 65;
-    cout << boolalpha << isOkay;
+
+    // Or operator
+    bool isOk = age > 18 || age < 65;
+
+    // Not operator
+    cout << boolalpha << !isOkay << endl
+         << isOk;
 }
