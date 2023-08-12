d0546acb5837b313b0fe8fff13fcfae079970856
ReDay Zarra <redayzarra@gmail.com>
2023-08-11 20:28:27 -0400

    Starting logical operator notes

--- Comparison.cpp
+++ Comparison.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int x = 10;
-    x >= 5; // Exactly like Python
+    // x >= 5; // Exactly like Python
 
     bool res = x != 5; 
     cout << boolalpha << res << endl;
