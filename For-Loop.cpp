6440b9a0ea1ea20efc39919c16c39951afc63acf
ReDay Zarra <redayzarra@gmail.com>
2023-08-14 05:28:04 -0400

    Finishing factorial exercise

--- For-Loop.cpp
+++ For-Loop.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main() {
-    for (int i = 0; i < 5; i++) {
-        cout << i;
+    // Reverse for-loop going from 5 to 1 inclusive
+    for (int i = 5; i > 0; i--) {
+        if (i % 2 != 0)
+            cout << i << endl;
     }
+
+    // Exercise - factorials
+    cout << "Enter your number :";
+    int num;
+    cin >> num;
+
+    int res = 1;
+    for (int i = 1; i <= num; i++) {
+        res *= i;
+    }
+    cout << res << endl;
 }
