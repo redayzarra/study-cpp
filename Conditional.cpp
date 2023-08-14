14254cdc7e9c3bde2c214b0a487de857dde7e91c
ReDay Zarra <redayzarra@gmail.com>
2023-08-13 20:46:57 -0400

    Finishing exercise for ternary conditional operators

--- Conditional.cpp
+++ Conditional.cpp
@@ -7,4 +7,22 @@ int main() {
     double commission = (sales > 10'000) ? 0.1 : 0.05;
 
     cout << commission;
+
+    // Exercise
+    cout << "Enter two values: ";
+    int first, second;
+
+    cin >> first >> second;
+
+    // Long way
+    int res;
+    if (first > second) {
+        res = first;
+    } else {
+        res = second;
+    }
+
+    // Concise way
+    res = (first > second) ? first : second;
+    cout << res;
 }
