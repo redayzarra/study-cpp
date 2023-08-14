df136ddc69392bafc888b11a01d527a887661d95
ReDay Zarra <redayzarra@gmail.com>
2023-08-14 05:05:59 -0400

    Learning about switch statements

--- Switch.cpp
+++ Switch.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 int main() {
-    
+    cout << "1 - Create Account" << endl    
+         << "2 - Change password" << endl
+         << "3 - Quit" << endl << endl
+         << "Select an option: " << endl;
+
+        short input;
+        cin >> input;
+
+        // If-Else statments to write the code:
+        
+        /* if (input == 1) 
+        *      cout << "You selected 1";
+        *  else if (input == 2) ... */
 }
