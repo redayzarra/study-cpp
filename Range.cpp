e86de15c3982205c036e4595d0efae84c725d34f
ReDay Zarra <redayzarra@gmail.com>
2023-08-16 20:13:00 -0400

    Finishing range exercise

--- Range.cpp
+++ Range.cpp
@@ -5,7 +5,24 @@ using namespace std;
 int main() {
     int numbers[] = {1, 2, 3};
 
-    for (int i = 0; i < 3; i++) {
-        cout << numbers[i] << endl;
+    // Iterate over every element in numbers
+    for (int number: numbers) {
+        cout << number << endl;
     }
+
+    // Iterate over every letter in string
+    string name = "ReDay";
+    for (char ch: name) {
+        cout << ch << endl;
+    }
+
+    int temp[] = {60, 90, 80};
+    int sum = 0;
+
+    for (int num: temp) {
+        sum += num;
+    }
+    double count = sizeof(temp) / sizeof(int);
+    double average = sum / count;
+    cout << average << endl;
 }
