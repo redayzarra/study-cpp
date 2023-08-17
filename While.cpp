fc5fe3fe32d10d418a8e1b3b4184d55b45920397
ReDay Zarra <redayzarra@gmail.com>
2023-08-17 19:59:07 -0400

    Learning while loop structure

--- While.cpp
+++ While.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
-    
+    for (int i = 1; i <= 5; i++) {
+        cout << i << endl;
+    }
+
+    int i = 1;
+    while (i <= 5) {
+        cout << i << endl;
+        i++;
+    }
 }
