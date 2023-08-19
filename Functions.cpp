ee74f2027145f41d5ef0439e283b9a7b02f696f7
ReDay Zarra <redayzarra@gmail.com>
2023-08-19 06:41:24 -0400

    Finshing exercise

--- Functions.cpp
+++ Functions.cpp
@@ -3,13 +3,22 @@
 using namespace std;
 
 void greet(string name) {
-    cout << "Hello, " + name + "!";
+    cout << "Hello, " + name + "!" << endl;
 }
 
 string fullName(string firstName, string lastName) {
     return firstName + " " + lastName;
 }
 
+//Exercise
+int sum(int x, int y) {
+    cout << x + y;
+    return 0;
+}
+
+
 int main() {
     greet(fullName("ReDay", "Zarra"));
+
+    sum(5, 6);
 }
