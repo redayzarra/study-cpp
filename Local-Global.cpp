1b6bf018fc254cce99a9b6d797442cebb0dfca78
ReDay Zarra <redayzarra@gmail.com>
2023-08-20 04:54:15 -0400

    Quick recap on local and global variables

---
+++ Local-Global.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// TaxRate is a global variable
+double taxRate = 0.2;
+
+int main() {
+
+    // Sales is a local variable
+    int sales = 10'000;
+}
