7038395ee6e840a39430ad28980257dcfd0091a9
ReDay Zarra <redayzarra@gmail.com>
2023-08-21 05:09:14 -0400

    Learning to avoid build problems

--- utils/great.hpp
+++ utils/great.hpp
@@ -1,6 +1,11 @@
+#ifndef UTILS_GREET
+#define UTILS_GREET
+
 #include <string>
 
 using namespace std;
 
 // Function declaration
 void greet(string name);
+
+#endif
---
+++ utils/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "utils/great.hpp"
+
+using namespace std;
+
+int main() {
+    greet("ReDay");
+}
