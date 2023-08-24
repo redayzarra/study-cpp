eb5186638843f8d385d62ccdfc23e6ec06f6d4c5
ReDay Zarra <redayzarra@gmail.com>
2023-08-23 20:56:18 -0400

    Learning to add array size along with array to function

---
+++ arrays/ArrayFunctions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// We always need to add the array's size as well
+void printNumbers(int numbers[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << numbers[i];
+    }
+}
+
+
+printNumbers({1, 2, 4, 6});
