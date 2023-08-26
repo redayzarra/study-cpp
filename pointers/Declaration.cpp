7298b7e087a626fe8e416acab51072a8ff635675
ReDay Zarra <redayzarra@gmail.com>
2023-08-25 21:29:14 -0400

    Finishing exercise with pointers

--- pointers/Declaration.cpp
+++ pointers/Declaration.cpp
@@ -6,6 +6,22 @@ int main() {
     int number = 10;
     cout << &number << endl;
 
-    // Declaring an int pointer
+    // Declaring an int pointer: address-of operator
     int* pointer = &number;
+
+    // Pointing a pointer to null - prints 0
+    int* ptr = nullptr;
+    cout << ptr << endl;
+
+    // Correct the exercise
+    int x = 10;
+    int y = 20;
+    ptr = &x;   // Assign the address of x to ptr
+    *ptr *= 2;  // Double the value pointed by ptr (which is x)
+    ptr = &y;   // Assign the address of y to ptr
+    *ptr *= 3;  // Triple the value pointed by ptr (which is y)
+
+    cout << x << ", " << y << endl;
+
+    return *pointer;
 }
