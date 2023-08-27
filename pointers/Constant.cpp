b321c1244f8b7132b6f2e51bb4a619f8ef19e395
ReDay Zarra <redayzarra@gmail.com>
2023-08-26 22:42:03 -0400

    Learning about constant integers

--- pointers/Constant.cpp
+++ pointers/Constant.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main() {
 
-    // Integer pointer cannot point to a constant value
-    int x = 10;
-    int* ptr = &x;
+    // Integer pointer cannot point to a different type including const
+    const int x = 10;
+    const int* ptr = &x;
+
+    // Not const pointer
+    int y = 20;
+    ptr = &y;
+
+    // Constant pointer pointing to constant integer
+    const int z = 30;
+    const int* const pointer = &x;
 }
