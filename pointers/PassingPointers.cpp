44d5fa7a7a7dda24f5d5b78cf90cbe9c084bddcb
ReDay Zarra <redayzarra@gmail.com>
2023-08-28 15:37:32 -0400

    Finishing exercise

--- pointers/PassingPointers.cpp
+++ pointers/PassingPointers.cpp
@@ -7,8 +7,21 @@ void increasePrice(double& price) {
     price *= 1.2;
 }
 
+// Exercise - swapping values of pointers
+void swap(int* first, int* second) {
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
 int main() {
     double price = 100;
     increasePrice(price);
-    cout << price;
+    cout << price << endl;
+
+    // Checking exercise 
+    int x = 10;
+    int y = 20;
+    swap(&x, &y);
+    cout << x << ", " << y;
 }
