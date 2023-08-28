338f6024d8d2ad367b62c274ffe972eb7491233c
ReDay Zarra <redayzarra@gmail.com>
2023-08-28 15:32:50 -0400

    Modern and preferred way to pass references to parameters

--- pointers/PassingPointers.cpp
+++ pointers/PassingPointers.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+// Function accepts type double called price - reference to the parameter
+void increasePrice(double& price) {
+    price *= 1.2;
+}
+
 int main() {
-    
+    double price = 100;
+    increasePrice(price);
+    cout << price;
 }
