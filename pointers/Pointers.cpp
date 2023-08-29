c223900eac039444f3a4ac1928174279d9c2ae96
ReDay Zarra <redayzarra@gmail.com>
2023-08-29 13:50:43 -0400

    Compiler always passes arrays using pointers

--- pointers/Pointers.cpp
+++ pointers/Pointers.cpp
@@ -2,10 +2,16 @@
 
 using namespace std;
 
+void printNumbers(int numbers[]) {
+    numbers[0] = 0;
+}
+
 int main() {
     int numbers[] = {10, 20, 30};
 
     // We will see the address of the first element of the array
     int* ptr = numbers;
     cout << ptr;
+    printNumbers(numbers);
+    cout << numbers[0];
 }
