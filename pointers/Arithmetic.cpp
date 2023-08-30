d90856e3d8b9f6684ad3f49999d3b07acf3f994f
ReDay Zarra <redayzarra@gmail.com>
2023-08-30 19:49:16 -0400

    Learning pointer arithmetic

---
+++ pointers/Arithmetic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numbers[] = {10, 20, 30};
+    int* ptr = numbers;
+
+    // Increments the pointer to the next element of the array
+    ptr++;
+    cout << *(ptr * 2);
+    cout << ptr[1];
+
+    // Decrements the pointer to the previous element of the array
+    ptr--;
+
+
+    cout << *ptr;
+}
