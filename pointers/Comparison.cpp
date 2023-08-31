138d4e6fcb37fc5d088fdcbf502e236af8ad7fd0
ReDay Zarra <redayzarra@gmail.com>
2023-08-31 04:07:10 -0400

    Finishing exercise

--- pointers/Comparison.cpp
+++ pointers/Comparison.cpp
@@ -10,8 +10,15 @@ int main() {
     int* ptrY = &y;
 
     if (*ptrX < *ptrY) {
-        cout << *ptrY;
+        cout << *ptrY << endl;
     }
 
     // Exercise
+    int numbers[] = {10, 20, 30};
+    int* ptr = &numbers[size(numbers) - 1];
+
+    while (ptr >= numbers) {
+        cout << *ptr << endl;
+        ptr--;
+    }
 }
