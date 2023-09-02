91367f7799a544fcd0346ae480e035de7815599b
ReDay Zarra <redayzarra@gmail.com>
2023-09-01 23:05:34 -0400

    Creating my own dynamic array using static arrays

--- pointers/Resizing.cpp
+++ pointers/Resizing.cpp
@@ -12,6 +12,20 @@ int main() {
 
         if (cin.fail()) break;
         entries++;
+
+        if (entries == 5) {
+            // Create a temp array 
+            int* temp = new int[10];
+
+            // Copy all elements to temp array
+            for (int i = 0; i < entries; i++) {
+                temp[i] = numbers[i];
+            }
+
+            // Deallocate and reassign numbers
+            delete[] numbers;
+            numbers = temp;
+        }
     }
 
     for (int i = 0; i < entries; i++) {
